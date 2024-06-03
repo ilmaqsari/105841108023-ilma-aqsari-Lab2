86a27c06b97e065e3775ba3ad5965121314cc6de
ilmaqsari <149998886+ilmaqsari@users.noreply.github.com>
2024-06-03 12:15:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//document.write("ini adalah kode javascript pertama ilma comel awwch")
+function changeContent() {
+    var element = document.getElementById("content");
+    element.innerHTML = "i lope you!";
+    document.getElementById("awwch").src = "https://i.pinimg.com/564x/61/a9/18/61a91802b9d2aaf12e13e49cc6e9fb81.jpg" ;
+}
